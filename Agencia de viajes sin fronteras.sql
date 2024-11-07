02c13213a0fc301ca7b6fc74d16ba65e0d338086
Ariel200312 <zitrortiz665@gmail.com>
2024-11-06 19:27:22 -0500

    Primer Commit

---
+++ Agencia de viajes sin fronteras.sql
@@ -0,0 +1,12 @@
+DROP TABLE clientes;
+CREATE TABLE clientes (
+id_cliente INT PRIMARY KEY,
+nombre VARCHAR (50),
+apellido VARCHAR (50),
+telefono VARCHAR (15),
+email VARCHAR (60),
+direccion VARCHAR (60), 
+ciudad VARCHAR (50), 
+pais VARCHAR (50)
+)
+
